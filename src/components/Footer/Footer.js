3fb54e8ac62070fa23dd17db3fd240f25960f3a1
Diksha Dhote <dikshadhote7042@gmail.com>
2022-03-19 01:45:46 +0530

    fix:copyright text alignment

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,19 +2,21 @@ import React from "react";
 import "./Footer.css";
 export default function Footer() {
   return (
-    <footer class="d-flex flex-justify-around mt-2 footer-ecom">
-      <div class="ml-3">
-        <p class="p-1">© 2021, All rights reserved</p>
-        <p class="pl-1">
+    <footer className="d-flex flex-justify-around mt-2 footer-ecom">
+      <div className="ml-3 rights-align">
+        <p className="pl-1">© 2022, All rights reserved</p>
+        <p className="pl-1">
           Made with{" "}
-          <span class="material-icons heart-icon icon-red"> favorite </span> by
-          Diksha Dhote
+          <span className="material-icons heart-icon icon-red"> favorite </span>
+          by Diksha Dhote
         </p>
       </div>
-      <div class="mr-3">
-        <h4 class="pl-1">Contact Us</h4>
-        <p class="p-1">Location: Central bazar road, HB Town, Nagpur - 44604</p>
-        <p class="pl-1">Call Us: (+91) 1800 0000 99</p>
+      <div className="mr-3">
+        <h4 className="pl-1">Contact Us</h4>
+        <p className="p-1">
+          Location: Central bazar road, HB Town, Nagpur - 44604
+        </p>
+        <p className="pl-1">Call Us: (+91) 1800 0000 99</p>
       </div>
     </footer>
   );

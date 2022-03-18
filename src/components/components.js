6cf4b992a6e5b5f0fd3c9af0e09178f1dd2e082a
Diksha Dhote <dikshadhote7042@gmail.com>
2022-03-18 18:10:25 +0530

    feat:components availablity in one file

---
+++ src/components/components.js
@@ -0,0 +1,10 @@
+import { Cart } from "./Cart/Cart";
+import { Footer } from "./Footer/Footer";
+import { Home } from "./Home/Home";
+import { Login } from "./Login/Login";
+import { Navbar } from "./Navbar/Navbar";
+import { Products } from "./Products/Products";
+import { Signup } from "./Signup/Signup";
+import { Wishlist } from "./Wishlist/Wishlist";
+
+export { Cart, Footer, Home, Login, Navbar, Products, Signup, Wishlist };

e4b806aa9741183ce199e398761da036319f4b8e
Diksha Dhote <dikshadhote7042@gmail.com>
2022-03-18 21:16:27 +0530

    feat:Navbar using Link component of react-router-dom

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,44 @@
 import React from "react";
+import "./../../App.css";
+import { Link } from "react-router-dom";
+import storeLogo from "../../assets/location.png";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      {" "}
+      <div className="nav-bar nav-yellow-shadow">
+        <div className="nav-logo-section">
+          <img src={storeLogo} className="mb-1" />
+          <h3 className="mr-4 aqua-color">dreamStore</h3>
+          <Link className="black-text-color" to="/products">
+            <h5>products</h5>
+          </Link>
+        </div>
+        <div className="search-bar-section">
+          <div className="search-container">
+            <input placeholder="search" className="search-bar" />
+            <span className="material-icons"> search </span>
+          </div>
+        </div>
+        <div className="nav-items">
+          <Link className="black-text-color" to="/wishlist">
+            <span className="material-icons mr-1" title="wishlist">
+              favorite
+            </span>
+          </Link>
+          <Link className="black-text-color" to="/cart">
+            <span className="material-icons mr-1 ml-1" title="cart">
+              shopping_cart
+            </span>
+          </Link>
+          <Link className="black-text-color" to="/login">
+            <span className="material-icons ml-1" title="account">
+              account_circle
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }

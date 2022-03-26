fcd10f4f893970df9a2789a058f88f82753250ce
Diksha Dhote <dikshadhote7042@gmail.com>
2022-03-27 00:24:32 +0530

    feat: sort by rating

--- src/utils/sort-functions.js
+++ src/utils/sort-functions.js
@@ -1,43 +1,39 @@
-const sortByPrice = (data, sortBy) => {
-  const sortArray = [...data];
-  console.log(sortArray);
-  if (sortBy === "high-to-low") {
-    const arr = sortArray.sort((item1, item2) => {
-      console.log("item1", item1.discountPrice);
-      console.log("item2", item2.discountPrice);
-      return Number(item2.discountPrice) - Number(item1.discountPrice);
-    });
-    return arr;
+const sortByPrice = (data, stateProduct) => {
+  if (data !== [] || data != undefined) {
+    const sortArray = [...data];
+    if (stateProduct.sortBy === "high-to-low") {
+      const arr = sortArray.sort((item1, item2) => {
+        return Number(item2.discountPrice) - Number(item1.discountPrice);
+      });
+      return arr;
+    }
+    if (stateProduct.sortBy === "low-to-high") {
+      return sortArray.sort(
+        (item1, item2) =>
+          Number(item1.discountPrice) - Number(item2.discountPrice)
+      );
+    }
+    return sortArray;
   }
+};
 
-  if (sortBy === "low-to-high") {
-    return sortArray.sort(
-      (item1, item2) =>
-        Number(item1.discountPrice) - Number(item2.discountPrice)
-    );
+const sortByRating = (data, stateProduct) => {
+  const sortArray = [...data];
+  const { sortRating } = stateProduct;
+  switch (sortRating) {
+    case 4:
+      return sortArray.filter((item) => Number(item.rating) >= 4);
+    case 3:
+      return sortArray.filter((item) => Number(item.rating) >= 3);
+    case 2:
+      return sortArray.filter((item) => Number(item.rating) >= 2);
+    case 1:
+      return sortArray.filter((item) => Number(item.rating) >= 1);
+    default:
+      return sortArray;
   }
-  return sortArray;
 };
 
 const sortByCategory = (data, category) => {};
-
-const sortByRating = (data, rating) => {
-  if (rating === "4 stars & above")
-    return data.filter((item) => {
-      item.rating === "4 stars & above" ? item : null;
-    });
-  if (rating === "3 stars & above")
-    return data.filter((item) => {
-      item.rating === "4 stars & above" ? item : null;
-    });
-  if (rating === "2 stars & above")
-    return data.filter((item) => {
-      item.rating === "4 stars & above" ? item : null;
-    });
-  if (rating === "1 stars & above")
-    return data.filter((item) => {
-      item.rating === "4 stars & above" ? item : null;
-    });
-};
 
 export { sortByPrice, sortByCategory, sortByRating };

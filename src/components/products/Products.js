52ed06edbfaf99d3676c1d278767f07ea4ed1f38
Diksha Dhote <dikshadhote7042@gmail.com>
2022-04-02 13:10:22 +0530

    feat: dispatch product when added to cart

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useProductsFilter } from "../../context/products-filter-context";
+import { useCart } from "../../context/cart-context";
 import { Navbar, Footer } from "../components";
 import FilterProduct from "./FilterProduct";
-
+import { Link } from "react-router-dom";
 export default function Products() {
   const { filteredProducts } = useProductsFilter();
-
+  const value = useCart();
+  console.log(value);
   return (
     <div>
       <Navbar />
@@ -14,57 +16,55 @@ export default function Products() {
         <div className="m-2 ">
           <h3 className="pb-1 pl-2 ">Products ({filteredProducts.length})</h3>
           <div className="d-flex align-items-stretch main-container flex-wrap">
-            {filteredProducts.map(
-              (
-                {
-                  subtitle,
-                  productImg,
-                  categoryName,
-                  description,
-                  discountPrice,
-                  orignalPrice,
-                  rating,
-                },
-                index
-              ) => {
-                return (
-                  <div
-                    className="card flex-column card-vert card-shadow"
-                    key={index}
-                  >
-                    <span className="badge add-to-fav" title="Add to Wishlist">
-                      <span className="material-icons icon-red">
-                        {" "}
-                        favorite{" "}
-                      </span>
-                    </span>
-                    <img src={productImg} className="card-img-vert" />
-                    <div className="card-body">
-                      <a className="card-title" href="#">
-                        {subtitle}
-                      </a>
-                      <span className="card-subtitle">{categoryName}</span>
-                      <p className="card-text">{description}</p>
-                      <div className="price-container">
-                        <span className="orignal-price">{orignalPrice}</span>
-                        <span className="discount-price">
-                          Rs {discountPrice}
-                        </span>
-                      </div>
-                      <button
-                        href="#"
-                        className="btn persian-blue-bg white-text-color btn-card btn-size"
-                      >
-                        <span className="material-icons card-btn-icon">
-                          shopping_cart
-                        </span>
-                        <p>Move to cart</p>
-                      </button>
+            {filteredProducts.map((product) => {
+              const {
+                _id,
+                subtitle,
+                productImg,
+                categoryName,
+                description,
+                discountPrice,
+                orignalPrice,
+                rating,
+              } = product;
+              return (
+                <div
+                  className="card flex-column card-vert card-shadow"
+                  key={_id}
+                >
+                  <span className="badge add-to-fav" title="Add to Wishlist">
+                    <span className="material-icons icon-red"> favorite </span>
+                  </span>
+                  <img src={productImg} className="card-img-vert" />
+                  <div className="card-body">
+                    <a className="card-title" href="#">
+                      {subtitle}
+                    </a>
+                    <span className="card-subtitle">{categoryName}</span>
+                    <p className="card-text">{description}</p>
+                    <div className="price-container">
+                      <span className="orignal-price">{orignalPrice}</span>
+                      <span className="discount-price">Rs {discountPrice}</span>
                     </div>
+                    <Link
+                      className="btn persian-blue-bg white-text-color btn-card btn-size"
+                      to="/cart"
+                      onClick={() =>
+                        dispatchCart({
+                          type: "ADD_TO_CART",
+                          payload: product,
+                        })
+                      }
+                    >
+                      <span className="material-icons card-btn-icon">
+                        shopping_cart
+                      </span>
+                      Move to cart
+                    </Link>
                   </div>
-                );
-              }
-            )}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>

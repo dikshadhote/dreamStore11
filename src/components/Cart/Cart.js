94409cc9f78b2b9ebe3f69598ffca36d8fa42278
Diksha Dhote <dikshadhote7042@gmail.com>
2022-04-02 13:11:00 +0530

    feat: use product in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Navbar, Footer } from "../components";
+import { useCart } from "../../context/cart-context";
 export default function Cart() {
+  const { stateCart } = useCart();
+  console.log(stateCart);
   return (
     <div>
       <Navbar />

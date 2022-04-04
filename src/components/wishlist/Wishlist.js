5b610b466215c1c67beaa12103da6727e4da289b
Diksha Dhote <dikshadhote7042@gmail.com>
2022-04-04 22:36:10 +0530

    feat: wishlist items number display

--- src/components/wishlist/Wishlist.js
+++ src/components/wishlist/Wishlist.js
@@ -12,6 +12,9 @@ export default function Wishlist() {
     <div>
       <Navbar />
       <div className="m-2">
+        <div className="d-flex flex-justify-center">
+          <h3 className="pb-1 pl-2 ">My Wishlist ({wishlist.length}) </h3>
+        </div>
         {wishlist.length === 0 ? (
           <div className="d-flex flex-justify-center ">
             <div className="empty-cart empty-wishlist">
@@ -24,64 +27,68 @@ export default function Wishlist() {
             </div>
           </div>
         ) : (
-          <div className="d-flex align-items-stretch flex-wrap flex-justify-center">
-            {wishlist.map((item) => {
-              const {
-                categoryName,
-                _id,
-                subtitle,
-                description,
-                discountPrice,
-                orignalPrice,
-                productImg,
-              } = item;
-              return (
-                <div
-                  className="card flex-column card-vert card-shadow"
-                  key={_id}
-                >
-                  <span
-                    className="badge cancel-card"
-                    title="Remove from wishlist"
-                    onClick={() =>
-                      dispatchWishlist({
-                        type: "REMOVE_FROM_WISHLIST",
-                        payload: _id,
-                      })
-                    }
+          <div>
+            <div className="d-flex align-items-stretch flex-wrap flex-justify-center">
+              {wishlist.map((item) => {
+                const {
+                  categoryName,
+                  _id,
+                  subtitle,
+                  description,
+                  discountPrice,
+                  orignalPrice,
+                  productImg,
+                } = item;
+                return (
+                  <div
+                    className="card flex-column card-vert card-shadow"
+                    key={_id}
                   >
-                    <span className="material-icons"> cancel </span>
-                  </span>
-                  <img src={productImg} className="card-img-vert" />
-                  <div className="card-body">
-                    <a className="card-title" href="#">
-                      {subtitle}
-                    </a>
-                    <span className="card-subtitle">{categoryName}</span>
-                    <p className="card-text">{description}</p>
-                    <div className="price-container">
-                      <span className="orignal-price">Rs {orignalPrice}</span>
-                      <span className="discount-price">Rs {discountPrice}</span>
-                    </div>
-                    <Link
-                      className="btn persian-blue-bg white-text-color btn-card btn-size"
-                      to="/cart"
+                    <span
+                      className="badge cancel-card"
+                      title="Remove from wishlist"
                       onClick={() =>
-                        dispatchCart({
-                          type: "ADD_TO_CART",
-                          payload: item,
+                        dispatchWishlist({
+                          type: "REMOVE_FROM_WISHLIST",
+                          payload: _id,
                         })
                       }
                     >
-                      <span className="material-icons card-btn-icon">
-                        shopping_cart
-                      </span>
-                      Move to cart
-                    </Link>
+                      <span className="material-icons"> cancel </span>
+                    </span>
+                    <img src={productImg} className="card-img-vert" />
+                    <div className="card-body">
+                      <a className="card-title" href="#">
+                        {subtitle}
+                      </a>
+                      <span className="card-subtitle">{categoryName}</span>
+                      <p className="card-text">{description}</p>
+                      <div className="price-container">
+                        <span className="orignal-price">Rs {orignalPrice}</span>
+                        <span className="discount-price">
+                          Rs {discountPrice}
+                        </span>
+                      </div>
+                      <Link
+                        className="btn persian-blue-bg white-text-color btn-card btn-size"
+                        to="/cart"
+                        onClick={() =>
+                          dispatchCart({
+                            type: "ADD_TO_CART",
+                            payload: item,
+                          })
+                        }
+                      >
+                        <span className="material-icons card-btn-icon">
+                          shopping_cart
+                        </span>
+                        Move to cart
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         )}
       </div>

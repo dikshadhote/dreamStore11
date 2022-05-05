48986aea9669c822fff98fe2fb95e9730c46975b
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-05 23:49:25 +0530

    feat: private routes

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -9,13 +9,21 @@ import {
   MockmanComponent,
   Signup,
 } from "./../components/components";
+import PrivateRoute from "./PrivateRoute";
 export default function Router() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
-      <Route path="/wishlist" element={<Wishlist />} />
-      <Route path="/cart" element={<Cart />} />
+      <Route
+        path="/wishlist"
+        element={<PrivateRoute navigateToPath={<Wishlist />}></PrivateRoute>}
+      />
+      <Route
+        path="/cart"
+        element={<PrivateRoute navigateToPath={<Cart />}></PrivateRoute>}
+      />
+
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/mockman" element={<MockmanComponent />} />
